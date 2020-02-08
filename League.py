698191f0e9f55f308f5ef7694d96292b59599f17
SivWatt <andylee1013@gmail.com>
2020-02-08 19:30:37 +0800

    Add argument to script

--- League.py
+++ League.py
@@ -1,14 +1,13 @@
 import pyautogui
-import time
 import win32gui
+
+import time
 from random import seed
 from random import randint
 import pyperclip
 import os
 import sys
 from os import listdir
-from os.path import isfile, isdir, join
-
 
 teamY = [ 130, 165, 200, 235 ]
 enemyY = [ 310, 345, 380, 415, 450 ]
@@ -33,10 +32,6 @@ def myRandom():
 
 def reportAPlayer(posX, posY):
 	pyautogui.moveTo(posX, posY)
-	#reportButton = pyautogui.locateCenterOnScreen('./reportIcon/report4.PNG')
-	
-	#cwd = sys.argv[0]
-	#print(cwd)
 	reportIcons = listdir(cwd + "/reportIcon")
 		
 	for i in reportIcons:
@@ -45,62 +40,67 @@ def reportAPlayer(posX, posY):
 			break
 	
 	if reportButton:
-		print(reportButton)
 		pyautogui.moveTo(reportButton)
-		# bring league to the front 
-		if __name__ == "__main__":
-			results = []
-			top_windows = []
-			win32gui.EnumWindows(windowEnumerationHandler, top_windows)
-			for i in top_windows:
-				if "league of legend" in i[1].lower():
-					print(i) 
-					win32gui.ShowWindow(i[0], 5)
-					win32gui.SetForegroundWindow(i[0])
-					break
-		time.sleep(1)
+		#time.sleep(1)
 		#pyautogui.click(reportButton,clicks=10,interval=0.5,)
 		pyautogui.mouseDown(reportButton,button='left',duration=2.0)
 		pyautogui.mouseUp(reportButton,button='left')
 
 		#locate report check boxes
-		checkboxes = list(pyautogui.locateAllOnScreen('checkbox.PNG'))
+		checkboxes = list(pyautogui.locateAllOnScreen(cwd + '/' + 'checkbox.PNG'))
 		nl = len(checkboxes)
-		print(nl)
 
 		# get random numbers
 		rn = myRandom()
-
-		print(rn)
 
 		# click check boxes
 		for i in rn:
 			pyautogui.click(checkboxes[i])
 
-		commentText = pyautogui.locateCenterOnScreen('comment.PNG')
+		commentText = pyautogui.locateCenterOnScreen(cwd + '/' + 'comment.PNG')
 		if commentText:
 			pyautogui.click(commentText)
-			#pyautogui.typewrite("asjadlasdk")
 			pyperclip.copy('外掛')
 			pyautogui.hotkey('ctrl','v')
 
-		reportConfirm = pyautogui.locateCenterOnScreen('reportConfirm.PNG')
+		reportConfirm = pyautogui.locateCenterOnScreen(cwd + '/' + 'reportConfirm.PNG')
 		if reportConfirm:
 			pyautogui.moveTo(reportConfirm)
-			pyautogui.click(reportConfirm)
+			#pyautogui.click(reportConfirm)
 
-		#cancel = pyautogui.locateCenterOnScreen('cancel.PNG')
-		#if cancel:
-		#	pyautogui.click(cancel)
+		# cancel report for testing
+		cancel = pyautogui.locateCenterOnScreen('cancel.PNG')
+		if cancel:
+			pyautogui.click(cancel)
 
-	else:
-		print("not found")
 
 #################################################
 # main script: 
 
-# locate aram icon
+# bring league to the front 
+if __name__ == "__main__":
+	results = []
+	top_windows = []
+	win32gui.EnumWindows(windowEnumerationHandler, top_windows)
+	for i in top_windows:
+		if "league of legend" in i[1].lower():
+			win32gui.ShowWindow(i[0], 9)
+			win32gui.SetForegroundWindow(i[0])
+			break
 
+# get argument to know how to report
+if len(sys.argv) > 1:
+	reportee = sys.argv[1]
+	if reportee == "ALL":
+		ys = teamY + enemyY
+	elif reportee == "TEAM":
+		ys = teamY
+	elif reportee == "ENEMY":
+		ys = enemyY
+else:
+	ys = enemyY
+
+# locate aram icon
 AramIconFiles = listdir(cwd + "/AramIcon")
 
 for i in AramIconFiles:
@@ -109,27 +109,7 @@ for i in AramIconFiles:
 		break
 
 if AramIcon:
-	#print(AramIcon)
-	#y = 130
-	ys = enemyY
-
 	for y in ys:
 		reportAPlayer(AramIcon.x, AramIcon.y + y)
-
-	#reportAPlayer(AramIcon.x, AramIcon.y + 130)
-	#reportAPlayer(AramIcon.x, AramIcon.y + 165)
-	#reportAPlayer(AramIcon.x, AramIcon.y + 200)
-	#reportAPlayer(AramIcon.x, AramIcon.y + 235)
-	#reportAPlayer(AramIcon.x, AramIcon.y + 310)
-	#reportAPlayer(AramIcon.x, AramIcon.y + 345)
-	#reportAPlayer(AramIcon.x, AramIcon.y + 380)
-	#reportAPlayer(AramIcon.x, AramIcon.y + 415)
-	#reportAPlayer(AramIcon.x, AramIcon.y + 450)
-else:
-	print("Aram Icon not found")
-
-
-
-
 
 #################################################
