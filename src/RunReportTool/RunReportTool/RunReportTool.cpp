dd85833ed6dcc19554cb4864921153ec4cb59b07
SivWatt <andylee1013@gmail.com>
2020-02-11 01:54:35 +0800

    Import RunReportTool.exe source.

---
+++ src/RunReportTool/RunReportTool/RunReportTool.cpp
@@ -0,0 +1,21 @@
+﻿// RunReportTool.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+//#include "pch.h"
+#include <iostream>
+#include <filesystem>
+#include <sstream>
+
+using namespace std;
+namespace fs = filesystem;
+
+int main(int argc, const char * argv[])
+{
+	fs::path currPath = fs::current_path();
+	string strCmd = "start /b pythonw ";
+
+	strCmd += currPath.u8string();
+	strCmd += "\\League.py";
+
+	system( strCmd.c_str() );
+}
