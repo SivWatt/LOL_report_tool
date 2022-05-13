a7d3d0e1af7b3f44b2bf68b60df2ef608c8c7632
SivWatt <andylee1013@gmail.com>
2022-05-14 03:01:38 +0800

    feat: modify to catch up new ui

--- league.py
+++ league.py
@@ -8,114 +8,121 @@ from random import seed
 from random import randint
 import time
 
+# client window size 
+# 1024 x 576
+# 1280 x 720 -- currently used
+# 1600 x 900
+
 class Reporter:
 	def __init__(self, logger, images, cwd):
 		self.logger = logger
 		self.images = images
 		with open(os.path.join(cwd, 'reportText.txt'), encoding='utf8') as file:
 			self.reportText = file.read()
-		self.teamY = [ 130, 165, 200, 235 ]
-		self.enemyY = [ 310, 345, 380, 415, 450 ]
 
-	def findGameModeIcon(self, leagueRegion):
-		#self.logger.info(leagueRegion)
-		for im in self.images.gameModeIcons:
-			#self.logger.info(im)
-			icon = pyautogui.locateCenterOnScreen(im, region=leagueRegion, confidence=0.9)
-			if icon:
-				#self.logger.info(icon)
-				self.logger.info("find game mode icon")
-				return icon
+		self.cancel_1280_720 = pyautogui.Point(820, 88)
+		self.team_1280_720 = [	
+								pyautogui.Point(280, 150), 
+								pyautogui.Point(280, 190), 
+								pyautogui.Point(280, 230), 
+								pyautogui.Point(280, 270), 
+								pyautogui.Point(280, 310), 
+							 ]
+		self.enemy_1280_720 = [	
+								pyautogui.Point(280, 395), 
+								pyautogui.Point(280, 435), 
+								pyautogui.Point(280, 475), 
+								pyautogui.Point(280, 515), 
+								pyautogui.Point(280, 555), 
+							 ]
 
 	def reportTeam(self):
 		self.logger.info("report teammates")
-		self.report(self.teamY)
+		client = window.LeagueClient()
+		clientSize = client.getSize()
+		client.bringFront()
+
+		if clientSize == (1280, 720):
+			self.report(client, self.team_1280_720)
+		else:
+			self.logger.warning("not supported window size: %s", clientSize)
 
 	def reportEnemy(self):
 		self.logger.info("report enemies")
-		self.report(self.enemyY)
+		client = window.LeagueClient()
+		clientSize = client.getSize()
+		client.bringFront()
+
+		if clientSize == (1280, 720):
+			self.report(client, self.enemy_1280_720)
+		else:
+			self.logger.warning("not supported window size: %s", clientSize)
 
 	def reportAll(self):
 		self.logger.info("report all other players")
 		self.reportTeam()
 		self.reportEnemy()
 
-	def report(self, targets):
-		client = window.LeagueClient()
-		client.bringFront()
-		leagueIcon = self.findGameModeIcon(client.rect)
-
+	def report(self, client, targets):
 		self.logger.info('report() >>>')
-		if leagueIcon:
-			for y in targets:
-				self.reportAPlayer(client.rect, leagueIcon.x, leagueIcon.y + y)
-		else:
-			self.logger.info('[report] League icon is not found')
+		self.logger.info("client: %s", client.rect)
+		for point in targets:
+			self.reportAPlayer(client.rect, point)
 		self.logger.info('report() <<<')
 
-	def reportAPlayer(self, leagueRegion, posX, posY):
-		pyautogui.moveTo(posX, posY)
-			
-		for im in self.images.reportButtons:
-			reportButton = pyautogui.locateCenterOnScreen(im, region=(posX, posY - 20, 300, 100), confidence=0.8, grayscale=True)
-			if reportButton:
-				break
+	def reportAPlayer(self, leagueRegion, point):
+		# move directly to the report button
+		reportPoint = pyautogui.Point(leagueRegion[0] + point.x, leagueRegion[1] + point.y + 20)
+		self.logger.info("move mouse to report button: %s", reportPoint)
+
+		pyautogui.moveTo(reportPoint)
+		pyautogui.mouseDown(reportPoint, button='left', duration=1.0)
+		pyautogui.mouseUp(reportPoint, button='left')
 		
-		if reportButton:
-			#pyautogui.moveTo(reportButton)
-			pyautogui.mouseDown(reportButton, button='left', duration=1.0)
-			pyautogui.mouseUp(reportButton, button='left')
-			time.sleep(0.1)
+		self.logger.info("locate check boxes")
+		# locate report check boxes
+		checkboxes = list(pyautogui.locateAllOnScreen(self.images.checkbox, region=leagueRegion, confidence=0.7))
+		
+		if checkboxes:
+			# get random numbers
+			rn = myRandom()
 
-			#locate report check boxes
-			checkboxes = list(pyautogui.locateAllOnScreen(self.images.checkbox, region=leagueRegion, confidence=0.7))
-			
-			if checkboxes:
-				# get random numbers
-				rn = myRandom()
+			# click check boxes
+			for i in rn:
+				pyautogui.click(checkboxes[i])
+		
+			# paste report text to comment text field
+			commentTextField = pyautogui.locateCenterOnScreen(self.images.commentText, region=leagueRegion)
+			if commentTextField:
+				pyautogui.click(commentTextField)
+				pyperclip.copy(self.reportText)
+				pyautogui.hotkey('ctrl','v')
 
-				# click check boxes
-				for i in rn:
-					pyautogui.click(checkboxes[i])
-			
-				# paste report text to comment text field
-				commentTextField = pyautogui.locateCenterOnScreen(self.images.commentText, region=leagueRegion)
-				if commentTextField:
-					pyautogui.click(commentTextField)
-					pyperclip.copy(self.reportText)
-					pyautogui.hotkey('ctrl','v')
+			# cancel report for testing
+			# cancel = pyautogui.Point(self.cancel_1280_720.x + leagueRegion[0], self.cancel_1280_720.y + leagueRegion[1])
+			# self.logger.info(cancel)
+			# if cancel:
+			# 	pyautogui.click(cancel)
+			# 	return
 
-				# cancel report for testing
-				# cancel = pyautogui.locateCenterOnScreen(self.images.cancelButton, region=leagueRegion, confidence=0.9)
-				# self.logger.info(self.images.cancelButton)
-				# self.logger.info(cancel)
-				# if cancel:
-				# 	pyautogui.click(cancel)
-				# 	return
-
-				#press report confirm button
-				reportConfirmRetry = 3
-				while reportConfirmRetry > 0:
-					reportConfirm = pyautogui.locateCenterOnScreen(self.images.reportConfirm, region=leagueRegion)
-					if reportConfirm:
-						pyautogui.moveTo(reportConfirm)
-						pyautogui.click(reportConfirm)
-						break
-					else:
-						self.logger.debug("report confirm button not found, wait for 1 second to retry. %d left" % reportConfirmRetry)
-						reportConfirmRetry = reportConfirmRetry - 1
-						time.sleep(1)
-					pass
-			else:
-				self.logger.info('checkbox is not detected')
-				# cancel report and go on.
-				cancel = pyautogui.locateCenterOnScreen(self.images.cancelButton, region=leagueRegion, confidence=0.9)
-				if cancel:
-					pyautogui.click(cancel)
-					return
+			#press report confirm button
+			reportConfirmRetry = 3
+			while reportConfirmRetry > 0:
+				reportConfirm = pyautogui.locateCenterOnScreen(self.images.reportConfirm, region=leagueRegion)
+				if reportConfirm:
+					pyautogui.moveTo(reportConfirm)
+					pyautogui.click(reportConfirm)
+					break
+				else:
+					self.logger.debug("report confirm button not found, wait for 1 second to retry. %d left" % reportConfirmRetry)
+					reportConfirmRetry = reportConfirmRetry - 1
+					time.sleep(1)
+				pass
 		else:
-			self.logger.debug('[reportAPlayer] report fail')
-
+			self.logger.info('checkbox is not detected')
+			# cancel report and go on.
+			cancel = pyautogui.Point(self.cancel_1280_720.x + leagueRegion[0], self.cancel_1280_720.y + leagueRegion[1])
+			pyautogui.click(cancel)
 
 def myRandom():
 	seed(time.time())
